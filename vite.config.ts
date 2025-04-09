84452a3af045713f962c47c7072c012a16095acd
Hu-chih-yao <jj2095813@gmail.com>
2025-04-09 20:01:53 +0800

    see

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
   server: {
     port: 8080
-  }
+  },
+  build: {
+    target: 'es2020',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
 });
